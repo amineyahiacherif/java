dca53cdc2e00b01d33b8f28d3ede536ffed3c9ed
amine <amine@example.com>
2025-10-30 16:52:11 +0000

    Update to version 2.2-SNAPSHOT

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World version 2.1!" );
+        System.out.println( "Hello World version 2.2 Build jenkins + uploead nexus" );
     }
 }
